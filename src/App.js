8608125e223a985e962cb5933fb20931b214ad05
Luciano Ojeda <lucianoojeda36@gmail.com>
2021-05-29 18:56:01 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import "./App.css";
+import Tilt from 'react-vanilla-tilt'
+
+export default function App() {
+  return (
+    <div className="container">
+      
+      
+      <Tilt className="box">
+        <h2 className="name"> Product Name </h2>
+        <a href="#" className="buy">
+          {" "}
+          Buy Now{" "}
+        </a>
+        <div className="circle"></div>
+        <img
+          src="http://pngimg.com/uploads/running_shoes/running_shoes_PNG5816.png"
+          className="product"
+        />
+      </Tilt>
+      
+       
+      
+      <Tilt className="box">
+        <h2 className="name"> Product Name </h2>
+        <a href="#" className="buy">
+          {" "}
+          Buy Now{" "}
+        </a>
+        <div className="circle"></div>
+        <img
+          src="http://pngimg.com/uploads/running_shoes/running_shoes_PNG5816.png"
+          className="product"
+        />
+      </Tilt>
+      
+      
+      <Tilt className="box">
+        <h2 className="name"> Product Name </h2>
+        <a href="#" className="buy">
+          {" "}
+          Buy Now{" "}
+        </a>
+        <div className="circle"></div>
+        <img
+          src="http://pngimg.com/uploads/running_shoes/running_shoes_PNG5816.png"
+          className="product"
+        />
+      </Tilt>
+      
+    </div>
+  );
+}
